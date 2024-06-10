03add175751c5417e7099c8686cd83da1d88a17e
Selam Nur <selamnur7@gmail.com>
2024-06-10 00:24:52 -0400

    all queries

--- queries.sql
+++ queries.sql
@@ -1,1 +1,4 @@
-SELECT * FROM vehicles;
+SELECT * FROM sales_contracts
+JOIN dealership
+	ON sales_contracts.id = dealership.dealership_id
+    WHERE date BETWEEN '2018-01-01' AND '2023-12-31' AND id = 5;
